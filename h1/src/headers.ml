dba4c5f1b39980fdd255b3c5fd0b69e7fdeaac37
Anurag Soni <anurag@sonianurag.com>
2021-05-12 20:59:27 -0400

    add find for headers

--- h1/src/headers.ml
+++ h1/src/headers.ml
@@ -6,3 +6,4 @@ let empty = []
 let of_list xs = xs
 let to_list headers = headers
 let iteri ~f headers = List.iter headers ~f:(fun (key, data) -> f ~key ~data)
+let find t f = List.Assoc.find ~equal:String.Caseless.equal t f
--- h1/src/headers.mli
+++ h1/src/headers.mli
@@ -6,3 +6,4 @@ val empty : t
 val of_list : (string, string) List.Assoc.t -> t
 val to_list : t -> (string, string) List.Assoc.t
 val iteri : f:(key:string -> data:string -> unit) -> t -> unit
+val find : t -> string -> string option
