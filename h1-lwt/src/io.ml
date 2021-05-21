988b9a3552ff4389fa303a3a97305b0b3c2c3567
Anurag Soni <anurag@sonianurag.com>
2021-05-21 16:23:49 -0400

    Deal with potentiall partially processed read buf
    Downstream consumers of the read stream can indicate "Partial" reads
    if no content was consumed from the buffer handed to them. We should
    detect such a scenario and attempt to refill the buffer before returning
    a response on the next pull on the read buffer stream.

--- h1-lwt/src/io.ml
+++ h1-lwt/src/io.ml
@@ -4,13 +4,25 @@ let fill refill read_buf =
 
 let reader_stream read_buf_size refill =
   let read_buf = Bigbuffer.create read_buf_size in
+  (* Use [prev_len] to keep track of whether any content was consumed from the
+     buffer during a run. If a stream as a non-zero length, but some content was
+     consumed in the previous run we return that right away. If no content was
+     consumed in a run, but the downstream consumer pulls on this stream again
+     we treat that as a scenario where the consumer needed more content than
+     what's available in the buffer right now. In such scenarios we attempt to
+     refill the buffer again and try again. *)
+  let prev_len = ref @@ Bigbuffer.length read_buf in
   let fn () =
     if Bigbuffer.length read_buf = Bigbuffer.capacity read_buf then
       Bigbuffer.resize read_buf 0;
-    if Bigbuffer.length read_buf > 0 then Lwt.return_some read_buf
-    else
+    if Bigbuffer.length read_buf > 0 && !prev_len <> Bigbuffer.length read_buf
+    then (
+      prev_len := Bigbuffer.length read_buf;
+      Lwt.return_some read_buf)
+    else (
+      prev_len := Bigbuffer.length read_buf;
       match%lwt fill refill read_buf with
       | `Eof -> Lwt.return_none
-      | `Ok -> Lwt.return_some read_buf
+      | `Ok -> Lwt.return_some read_buf)
   in
   Lstream.from_fn fn
--- h1-lwt/src/io.mli
+++ h1-lwt/src/io.mli
@@ -1,1 +1,4 @@
-val reader_stream : int -> (Bigstringaf.t -> pos:int -> len:int -> int Lwt.t) -> Bigbuffer.t Lstream.t
+val reader_stream :
+  int ->
+  (Bigstringaf.t -> pos:int -> len:int -> int Lwt.t) ->
+  Bigbuffer.t Lstream.t
