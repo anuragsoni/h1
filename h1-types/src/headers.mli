5d5c09041e4076eedd20d5f36783b2fd845d6e4e
Anurag Soni <anurag@sonianurag.com>
2021-05-21 23:25:29 -0400

    don't leave clients waiting for a 100-continue response (#9)
    https://datatracker.ietf.org/doc/html/rfc7231#section-5.1.1
    
    Http clients can send an Expect: 100-continue header for POST/PUT
    requests. This indicates that the client wishes to receive a 100
    (Continue) response before attempting to send (a potentially large)
    message body.
    
    TODO: we should allow users to configure what checks should be
    performed here and provide a way for them to respond with an
    Expectation failed response to exit early.
    
    Note: https://developer.mozilla.org/en-US/docs/Web/HTTP/Status/417

--- h1-types/src/headers.mli
+++ h1-types/src/headers.mli
@@ -8,3 +8,4 @@ val iteri : f:(key:string -> data:string -> unit) -> t -> unit
 val find : t -> string -> string option
 val find_multi : t -> string -> string list
 val get_transfer_encoding : t -> [ `Bad_request | `Chunked | `Fixed of int64 ]
+val client_waiting_for_100_continue : t -> bool
